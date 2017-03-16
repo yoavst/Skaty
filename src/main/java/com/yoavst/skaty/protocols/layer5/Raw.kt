c3ba6be9e5ab26867964a00a78e15c75d88605ee
Yoav Sternberg <yoav.sternberg@gmail.com>
2017-03-16 20:08:13 +0200

    inline raw text

--- src/main/java/com/yoavst/skaty/protocols/layer5/Raw.kt
+++ src/main/java/com/yoavst/skaty/protocols/layer5/Raw.kt
@@ -2,21 +2,25 @@
 
 package com.yoavst.skaty.protocols
 
+import com.yoavst.skaty.model.Formatted
+import com.yoavst.skaty.model.Formatter
 import com.yoavst.skaty.protocols.declarations.IProtocol
 import com.yoavst.skaty.serialization.SerializationContext
 import com.yoavst.skaty.serialization.SimpleReader
 import com.yoavst.skaty.serialization.readAsByteArray
 import com.yoavst.skaty.utils.ToString
 
-data class Raw(var load: String = "") : IProtocol<Raw> {
+data class Raw(@property:Formatted(Companion::class) var load: String = "") : IProtocol<Raw> {
     override fun toString(): String = ToString.generate(this)
     override val marker get() = Companion
 
-    companion object : IProtocolMarker<Raw> {
+    companion object : IProtocolMarker<Raw>, Formatter<String> {
         override val name: String get() = "Raw"
         override fun isProtocol(protocol: IProtocol<*>): Boolean = protocol is Raw
         override val defaultValue: Raw = Raw()
 
         override fun of(reader: SimpleReader, serializationContext: SerializationContext): Raw? = Raw(String(reader.readAsByteArray(), Charsets.US_ASCII))
+
+        override fun format(value: String?): String = "\"${(value ?: "").replace("\r\n", "\\n").replace("\n", "\\n")}\""
     }
 }
