42fa41a86e4b22f69a430bef1bdf8f03081dc476
Yoav Sternberg <yoav.sternberg@gmail.com>
2017-05-26 16:51:33 +0300

    Switch to build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+buildscript {
+    repositories {
+        jcenter()
+    }
+    dependencies {
+        classpath(kotlinModule(module = "gradle-plugin", version = "1.1.2-2"))
+    }
+}
+
+apply {
+    plugin("kotlin")
+}
+
+repositories {
+    jcenter()
+}
+
+dependencies {
+    compile(kotlinModule(module ="stdlib", version = "1.1.2-2"))
+    compile(kotlinModule(module ="reflect", version = "1.1.2-2"))
+
+    compile("org.slf4j:slf4j-api:1.7.24")
+    compile("io.github.microutils:kotlin-logging:1.4.3")
+    testCompile("org.slf4j:slf4j-simple:1.7.24")
+
+    compile("org.pcap4j:pcap4j-core:1.+")
+    compile("org.pcap4j:pcap4j-packetfactory-static:1.+")
+}
